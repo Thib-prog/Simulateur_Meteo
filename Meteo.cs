89ce8db25b3b2eec5f989f2f29a6f1865ff049c2
Thibaut <thibaut.leyter@hotmail.fr>
2020-09-27 19:40:24 +0200

    add forgotten meteo class
    Signed-off-by: Thibaut <thibaut.leyter@hotmail.fr>

---
+++ Meteo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimulateurMeteo
+{
+    public class Meteo
+    {
+        public delegate void changementMeteoDel(MeteoEnum newMeteo);
+        public event changementMeteoDel ChangementMeteo;
+        public enum MeteoEnum
+        {
+            Soleil = 0,
+            Nuage,
+            Pluie,
+            Orage
+        }
+        public MeteoEnum MeteoActuel = 0;
+
+        public void CheckWeather(int value)
+        {
+            MeteoEnum newMeteo;
+            if (value < 5)
+                newMeteo = MeteoEnum.Soleil;
+            else if (value < 50)
+                newMeteo = MeteoEnum.Nuage;
+            else if (value < 90)
+                newMeteo = MeteoEnum.Pluie;
+            else
+                newMeteo = MeteoEnum.Orage;
+
+            if (MeteoActuel != newMeteo)
+            {
+                ChangementMeteo(newMeteo);
+            }
+            MeteoActuel = newMeteo;
+        }
+    }
+
+
+}
