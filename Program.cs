d7d5ee5a280f508622712362e3d452dd4e4f98eb
Thibaut <thibaut.leyter@hotmail.fr>
2020-09-27 17:59:42 +0200

    add creation of random array number

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace SimulateurMeteo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Combien de mesures doivent être générées?");
+            ConsoleKeyInfo key1 = Console.ReadKey();
+            if (!char.IsDigit(Convert.ToChar(key1)))
+            {
+                Console.WriteLine("Not a digit");
+                return;
+            }
+            int[] arrayRnd = new int[Convert.ToInt16(key1)];
+            Random rnd = new Random();
+            for (int i = 0; i < arrayRnd.Length; i++)
+            {
+                arrayRnd[i]=rnd.Next(0, 100);
+            }
+            Console.WriteLine(" Press any key to exit ...");
+            Console.ReadKey();
         }
+
+        
     }
 }
